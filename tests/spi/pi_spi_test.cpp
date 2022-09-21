6f44e46d07628ab9e731ee81d16f3475f87c77b7
saegersven <sven.saeger@hotmail.com>
2022-09-21 18:40:32 +0200

    Added untested SPI test

---
+++ tests/spi/pi_spi_test.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <wiringPiSPI.h>
+
+// Compile with
+// g++ -o pi_spi_test pi_spi_test.cpp -lwiringPi
+// Run with
+// ./pi_spi_test
+
+#define SPI_CHANNEL 0
+#define SPI_CLOCK_SPEED 1000000
+
+int main() {
+	int fd = wiringPiSPISetupMode(SPI_CHANNEL, SPI_CLOCK_SPEED, 0);
+	if(fd == -1) {
+		std::cout << "Failed to init SPI communication.\n";
+		return -1;
+	}
+	std::cout << "SPI init successful.\n";
+
+	unsigned char buf1[3] = { 1, 32, 0 };
+	unsigned char buf2[3] = { 2, 21, 0 };
+
+	wiringPiSPIDataRW(SPI_CHANNEL, buf1, 3);
+
+	std::cout << "Data returned: " << std::to_string(buf1[2]) << "\n";
+
+	wiringPiSPIDataRW(SPI_CHANNEL, buf2, 3);
+
+	std::cout << "Data returned: " << std::to_string(buf2[2]) << "\n";
+
+	return 0;
+}
