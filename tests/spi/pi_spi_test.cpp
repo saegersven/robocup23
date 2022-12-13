19a34278dc2b5a3edd0b76dad6a864eb5459ef1f
kraemerlukas314 <lukas.kraemer.2004@gmail.com>
2022-12-13 17:34:10 +0100

    working spi test

--- tests/spi/pi_spi_test.cpp
+++ tests/spi/pi_spi_test.cpp
@@ -2,17 +2,94 @@
 #include <wiringPiSPI.h>
 #include <wiringPi.h>
 #include <unistd.h>
+#include <cstring>
+
+extern "C" {
+	#include <fcntl.h>
+	#include <sys/ioctl.h>
+	#include <linux/spi/spidev.h>
+}
+
+static const char* device = "/dev/spidev0.0";
+static uint8_t mode = 0;
+static uint8_t bits = 8;
+static uint32_t speed = 500000;
+int ret, fd;
 
 // Compile with
 // g++ -o pi_spi_test pi_spi_test.cpp -lwiringPi
 // Run with
 // ./pi_spi_test
 
-#define SPI_CHANNEL 0
-#define SPI_CLOCK_SPEED 50000
-
 int main() {
-	wiringPiSetupGpio();
+	/* Device oeffen */
+	if ((fd = open(device, O_RDWR)) < 0)
+	{
+		std::cout << "Could not open device" << std::endl;
+	}
+	/* Mode setzen */
+	ret = ioctl(fd, SPI_IOC_WR_MODE, &mode);
+	if (ret < 0)
+	{
+		std::cout << "Could not set mode" << std::endl;
+	}
+
+	/* Mode abfragen */
+	ret = ioctl(fd, SPI_IOC_RD_MODE, &mode);
+	if (ret < 0)
+	{
+		std::cout << "Could not get mode" << std::endl;
+	}
+
+	/* Wortlaenge setzen */
+	ret = ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bits);
+	if (ret < 0)
+	{
+		std::cout << "Could not set bits" << std::endl;
+	}
+
+	/* Wortlaenge abfragen */
+	ret = ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &bits);
+	if (ret < 0)
+	{
+		std::cout << "Could not get bits" << std::endl;
+	}
+
+	/* Datenrate setzen */
+	ret = ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
+	if (ret < 0)
+	{
+		std::cout << "Could not set speed" << std::endl;
+	}
+	
+	/* Datenrate abfragen */
+	ret = ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &speed);
+	if (ret < 0)
+	{
+		std::cout << "Could not get speed" << std::endl;
+	}
+
+	srand(time(NULL));
+	unsigned char buf[3] = {rand() % 128, 123, 2};
+	std::cout << (int)buf[0] << std::endl;
+	spi_ioc_transfer spi;
+	spi.tx_buf = (unsigned long)buf;
+	//spi.rx_buf = (unsigned long)buf;
+	spi.len = 3;
+	spi.delay_usecs = 0;
+	spi.speed_hz = speed;
+	spi.bits_per_word = bits;
+	spi.cs_change = 0;
+
+	ret = ioctl(fd, SPI_IOC_MESSAGE(1), &spi);
+
+	if(ret < 0) {
+		std::cout << "Could not transfer" << std::endl;
+	}
+
+	return ret;
+
+	/*wiringPiSetupGpio();
 	pinMode(8, OUTPUT);
 	digitalWrite(8, HIGH);
 
@@ -23,12 +100,18 @@ int main() {
 	}
 	std::cout << "SPI init successful.\n";
 
-	unsigned char buf1[3] = { 42, 13, 42};
+	unsigned char buf1[3] = { 42, 24, 21};
 
-	digitalWrite(8, LOW);	
+	for(int i = -128; i < 128; ++i) {
+		unsigned char buf[3] = {2, 21, 43};
+		wiringPiSPIDataRW(SPI_CHANNEL, buf, 3);
+		usleep(1000000);
+	}*/
+
+	/*digitalWrite(8, LOW);	
 	wiringPiSPIDataRW(SPI_CHANNEL, buf1, 3);
 	digitalWrite(8, HIGH);	
-	usleep(100000);
+	usleep(100000);*/
 
 	return 0;
 }
--- tests/spi/teensy_spi_test/teensy_spi_test.ino
+++ tests/spi/teensy_spi_test/teensy_spi_test.ino
@@ -1,5 +1,4 @@
 //#include <interrupts.h>
-#include <TSPISlave.h>
 
 uint8_t miso = 12;
 uint8_t mosi = 11;
@@ -7,16 +6,14 @@ uint8_t sck = 13;
 uint8_t cs = 15;
 uint8_t spimode = 8;
 
-byte buffer[128];
+uint8_t buffer[128];
 uint8_t num_bits = 0;
-
-TSPISlave spi = TSPISlave(SPI, miso, mosi, sck, cs, spimode);
 
 void setup() {
   Serial.begin(115200);
 
   pinMode(cs, INPUT);
-  pinMode(miso, INPUT);
+  pinMode(mosi, INPUT);
   pinMode(sck, INPUT);
   attachInterrupt(sck, sck_rising_interrupt, RISING);
 
@@ -24,9 +21,9 @@ void setup() {
 }
 
 void sck_rising_interrupt() {
-    buffer[num_bits / 8] |= digitalRead(miso) << (7 - num_bits % 8);
-    ++num_bits; 
-    Serial.println(num_bits);
+    Serial.print(digitalRead(mosi));
+    buffer[num_bits / 8] |= digitalRead(mosi) << (7 - num_bits % 8);
+    ++num_bits;
 }
 
 
@@ -34,10 +31,16 @@ void loop() {
     while(digitalRead(cs) != LOW);
     while(digitalRead(cs) == LOW);
     int len = num_bits / 8;
+    num_bits = 0;
+    
+    Serial.println();
     for(int i = 0; i < len; ++i) {
-      Serial.print((int)buffer[i]);
+      Serial.print((uint8_t)buffer[i]);
       if(i != len - 1) Serial.print("\t");
     }
-    num_bits = 0;
-
+    Serial.println();
+    
+    for(int i = 0; i < len; ++i) {
+      buffer[i] = 0;
+    }
 }
