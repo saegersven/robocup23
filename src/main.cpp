27e020a5fe503242f6fc9cad2131f1569d3d7ae7
kraemerlukas314 <lukas.kraemer.2004@gmail.com>
2022-12-15 17:40:13 +0100

    test commit

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,6 @@ int main() {
 	std::shared_ptr<Robot> robot = std::make_shared<Robot>();
 	
 	/*while(true) {
-		//robot->m(50, 50, 500);
-		//robot->m(-50, -50, 500);
 		std::cout << robot->read_distance() << std::endl;
 	}*/
 
