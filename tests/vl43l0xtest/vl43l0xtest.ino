faaea61ae41961f1704a98873d0d623b3f3052c3
kraemerlukas314 <lukas.kraemer.2004@gmail.com>
2023-06-12 13:21:52 +0200

    vl53l0x test

--- tests/vl43l0xtest/vl43l0xtest.ino
+++ tests/vl43l0xtest/vl43l0xtest.ino
@@ -1,48 +1,42 @@
 #include <Wire.h>
 #include <VL53L0X.h>
+
 #define NUM_VL53L0X 2
-VL53L0X dist_sensors[NUM_VL53L0X];
 int dist_xshut_pins[NUM_VL53L0X] = {12, 11};
 int dist_addresses[NUM_VL53L0X] = {0x8A, 0x8B};
+VL53L0X dist_sensors[NUM_VL53L0X];
 
 void setup() {
   Serial.begin(115200);
-  Wire.begin();
-  
-  // shutdown sensors
-  pinMode(dist_xshut_pins[0], OUTPUT);
-  pinMode(dist_xshut_pins[1], OUTPUT);
-  delay(10);
-  digitalWrite(dist_xshut_pins[0], LOW);
-  digitalWrite(dist_xshut_pins[0], LOW);
-  delay(10);
 
-  Serial.println("Sensors shut down");
+  Wire.begin();
+
+  // Configure distance sensors
+  for (int i = 0; i < NUM_VL53L0X; ++i) {
+    pinMode(dist_xshut_pins[i], OUTPUT);
+    delay(10);
+    digitalWrite(dist_xshut_pins[i], LOW);
+  }
+  delay(10);
 
   for (int i = 0; i < NUM_VL53L0X; ++i) {
+
+    Serial.println("3.14");
     digitalWrite(dist_xshut_pins[i], HIGH);
     delay(10);
 
-    if (!dist_sensors[i].init()) {
-      Serial.print("Failed to initialize dist sensor ");
-      Serial.println(i);
-    }
+    if (!dist_sensors[i].init()) exit(-1);
 
     dist_sensors[i].setAddress(dist_addresses[i]);
     dist_sensors[i].setMeasurementTimingBudget(20000);
     dist_sensors[i].startContinuous();
-    dist_sensors[i].setTimeout(200);
+    dist_sensors[i].setTimeout(100);
     delay(10);
   }
 }
 
 void loop() {
-  long long starttime = millis();
   Serial.print(dist_sensors[0].readRangeContinuousMillimeters());
-  Serial.print("  ");
-  Serial.print(dist_sensors[1].readRangeContinuousMillimeters());
-  long long endtime = millis();
-  Serial.print("   | Took: ");
-  Serial.print((int)(endtime - starttime));
-  Serial.println("ms");
+  Serial.print("  |  ");
+  Serial.println(dist_sensors[1].readRangeContinuousMillimeters());
 }
